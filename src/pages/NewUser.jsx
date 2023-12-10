32f21d36f7b0d9e409426979cbe78462f922f0a4
JanesWorld <jayjanetjane@gmail.com>
2023-12-10 21:26:54 +0000

    update main page

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,108 +1,61 @@
-import { Box, Button, CardMedia, Container, IconButton } from "@mui/material";
-import React, { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const NewUser = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const mainImages = [
-    "./joinCommunity.png",
-    "./friendsmoments.png",
-    "./MainPage.png",
-  ];
-
-  const goToPrevious = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? mainImages.length - 1 : prevIndex - 1
-    );
-  };
-
-  const goToNext = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === mainImages.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
   let navigate = useNavigate();
   const handleCreateAccountClick = () => {
     navigate("/create-account");
   };
 
   return (
-    <Box sx={{ backgroundColor: "white" }}>
-      <Container
+    <Box>
+      <Box
         sx={{
+          position: "relative",
+          height: "75vh",
+          backgroundImage: "url(path_to_your_hero_image.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
           display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-end",
-          padding: "30px",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          color: "white",
         }}
       >
+        <Typography variant="h2" gutterBottom sx={{ color: "black" }}>
+          Connect with the World
+        </Typography>
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{ maxWidth: "500px", textAlign: "center", color: "black" }}
+        >
+          Join the community to share and discover amazing moments around the
+          globe.
+        </Typography>
         <Button
+          variant="contained"
+          color="primary"
+          size="large"
           sx={{
+            mt: 7,
+            borderWidth: "1px",
+            borderColor: "#1E555C",
             backgroundColor: "#1E555C",
             color: "white",
-            borderRadius: 2,
-            ":hover": {
-              bgcolor: "#F15152",
+            "&:hover": {
+              backgroundColor: "#F15152",
               color: "white",
+              borderColor: "#F15152",
             },
           }}
           onClick={handleCreateAccountClick}
         >
-          Create New Account
+          Join
         </Button>
-      </Container>
-      <Container
-        sx={{ padding: "10px", minHeight: "300px", textAlign: "center" }}
-      >
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <CardMedia
-            component="iframe"
-            height="300"
-            width="60%"
-            src="./promoVid.mp4"
-            title="Link Up Video"
-            sx={{ borderRadius: "8px" }}
-          />
-        </Box>
-      </Container>
-      <Container
-        sx={{ padding: "10px", position: "relative", textAlign: "center" }}
-      >
-        <IconButton
-          onClick={goToPrevious}
-          sx={{
-            position: "absolute",
-            left: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-            zIndex: 2,
-          }}
-        >
-          <ArrowBackIosIcon />
-        </IconButton>
-
-        <img
-          src={mainImages[currentImageIndex]}
-          alt={`Gallery ${currentImageIndex}`}
-          style={{ width: "100%", height: "auto" }}
-        />
-
-        <IconButton
-          onClick={goToNext}
-          sx={{
-            position: "absolute",
-            right: 0,
-            top: "50%",
-            transform: "translateY(-50%)",
-            zIndex: 2,
-          }}
-        >
-          <ArrowForwardIosIcon />
-        </IconButton>
-      </Container>
+      </Box>
     </Box>
   );
 };
